c4b1d91e19e212fbedce925b7573792694bba394
Vladimir Pechishev <pechishev.volodya@yandex.ru>
2017-05-04 19:10:56 +0300

    поправил тесты

--- Adapter/AdapterTests/CanvasClassAdapterTest.cpp
+++ Adapter/AdapterTests/CanvasClassAdapterTest.cpp
@@ -13,50 +13,57 @@ using namespace shape_drawing_lib;
 struct CanvasClassAdapter
 {
 	std::stringstream stream;
-	CCanvasClassAdapter adapter;
-	CanvasClassAdapter()
-		: adapter(stream)
-	{
-	}
 };
-// TODO: call destruct adapter
+
 BOOST_FIXTURE_TEST_SUITE(canvas_class_adapter_tests, CanvasClassAdapter)
 
 BOOST_AUTO_TEST_CASE(should_draw_line)
 {
-	adapter.MoveTo(0, 0);
-	adapter.LineTo(10, 10);
+	{
+		CCanvasClassAdapter adapter(stream);
+		adapter.MoveTo(0, 0);
+		adapter.LineTo(10, 10);
+	}
 	std::string expectedString = R"(<draw>
   <line fromX="0" fromY="0" toX="10" toY="10"/>
+</draw>
 )";
 	BOOST_CHECK_EQUAL(stream.str(), expectedString);
 }
 
 BOOST_AUTO_TEST_CASE(should_draw_rectangle)
 {
-	CCanvasPainter painter(adapter);
+	{
+		CCanvasClassAdapter adapter(stream);
+		CCanvasPainter painter(adapter);
 
-	CRectangle rect({ 0, 0 }, 50, 60);
-	painter.Draw(rect);
+		CRectangle rect({ 0, 0 }, 50, 60);
+		painter.Draw(rect);
+	}
 	std::string expectedString = R"(<draw>
   <line fromX="0" fromY="0" toX="50" toY="0"/>
   <line fromX="50" fromY="0" toX="50" toY="60"/>
   <line fromX="50" fromY="60" toX="0" toY="60"/>
   <line fromX="0" fromY="60" toX="0" toY="0"/>
+</draw>
 )";
 	BOOST_CHECK_EQUAL(stream.str(), expectedString);
 }
 
 BOOST_AUTO_TEST_CASE(should_draw_triangle)
 {
-	CCanvasPainter painter(adapter);
+	{
+		CCanvasClassAdapter adapter(stream);
+		CCanvasPainter painter(adapter);
 
-	CTriangle triangle({ 50, 0 }, { 0, 20 }, { 50, 20 });
-	painter.Draw(triangle);
+		CTriangle triangle({ 50, 0 }, { 0, 20 }, { 50, 20 });
+		painter.Draw(triangle);
+	}
 	std::string expectedString = R"(<draw>
   <line fromX="50" fromY="0" toX="0" toY="20"/>
   <line fromX="0" fromY="20" toX="50" toY="20"/>
   <line fromX="50" fromY="20" toX="50" toY="0"/>
+</draw>
 )";
 	BOOST_CHECK_EQUAL(stream.str(), expectedString);
 }
--- Adapter/AdapterTests/CanvasObjectAdapterTest.cpp
+++ Adapter/AdapterTests/CanvasObjectAdapterTest.cpp
@@ -15,51 +15,63 @@ struct CanvasObjectAdapter
 {
 	std::stringstream stream;
 	CModernGraphicsRenderer renderer;
-	CCanvasAdapter adapter;
+	
 	CanvasObjectAdapter()
 		: renderer(stream)
-		, adapter(renderer)
 	{
 	}
 };
-// TODO: call destruct adapter
+
 BOOST_FIXTURE_TEST_SUITE(canvas_object_adapter_tests, CanvasObjectAdapter)
 
 BOOST_AUTO_TEST_CASE(should_draw_line)
 {
-	adapter.MoveTo(0, 0);
-	adapter.LineTo(10, 10);
+	{
+		CCanvasAdapter adapter(renderer);
+		adapter.MoveTo(0, 0);
+		adapter.LineTo(10, 10);
+	}
+	
 	std::string expectedString = R"(<draw>
   <line fromX="0" fromY="0" toX="10" toY="10"/>
+</draw>
 )";
 	BOOST_CHECK_EQUAL(stream.str(), expectedString);
 }
 
 BOOST_AUTO_TEST_CASE(should_draw_rectangle)
 {
-	CCanvasPainter painter(adapter);
+	{
+		CCanvasAdapter adapter(renderer);
+		CCanvasPainter painter(adapter);
 
-	CRectangle rect({0, 0}, 50, 60);
-	painter.Draw(rect);
+		CRectangle rect({ 0, 0 }, 50, 60);
+		painter.Draw(rect);
+	}
 	std::string expectedString = R"(<draw>
   <line fromX="0" fromY="0" toX="50" toY="0"/>
   <line fromX="50" fromY="0" toX="50" toY="60"/>
   <line fromX="50" fromY="60" toX="0" toY="60"/>
   <line fromX="0" fromY="60" toX="0" toY="0"/>
+</draw>
 )";
 	BOOST_CHECK_EQUAL(stream.str(), expectedString);
 }
 
 BOOST_AUTO_TEST_CASE(should_draw_triangle)
 {
-	CCanvasPainter painter(adapter);
+	{
+		CCanvasAdapter adapter(renderer);
+		CCanvasPainter painter(adapter);
 
-	CTriangle triangle({ 50, 0 }, { 0, 20 }, {50, 20});
-	painter.Draw(triangle);
+		CTriangle triangle({ 50, 0 }, { 0, 20 }, { 50, 20 });
+		painter.Draw(triangle);
+	}
 	std::string expectedString = R"(<draw>
   <line fromX="50" fromY="0" toX="0" toY="20"/>
   <line fromX="0" fromY="20" toX="50" toY="20"/>
   <line fromX="50" fromY="20" toX="50" toY="0"/>
+</draw>
 )";
 	BOOST_CHECK_EQUAL(stream.str(), expectedString);
 }
